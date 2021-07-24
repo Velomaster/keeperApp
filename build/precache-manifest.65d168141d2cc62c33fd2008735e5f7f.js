4d4b8c834e1c7798d81b9883b6329c0b3ce5fc5a
Alexander Gudkov <alexandergudkov@Alexanders-MBP.attlocal.net>
2021-07-24 12:03:35 -0500

    minor fixes

---
+++ build/precache-manifest.65d168141d2cc62c33fd2008735e5f7f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e20e63bab4b375e0afb79e27ccdfed32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a129db8c9d86a23d8b5f",
+    "url": "/static/js/2.aa89c375.chunk.js"
+  },
+  {
+    "revision": "3a84fd1750a1de369ea1",
+    "url": "/static/js/main.80234f46.chunk.js"
+  },
+  {
+    "revision": "921d0da837860e5eca9c",
+    "url": "/static/js/runtime-main.0ce15bf1.js"
+  }
+]);
